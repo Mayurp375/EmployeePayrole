37a10f3fd3a1976e4d26a6886a198ffb9fba9011
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-06 17:44:26 +0530

    Use Lombok Library for Logging,using Annotation @Slf4j at Class Level,         Logging is activated

--- src/main/java/com/employeepayrole/EmployeePayroleApplication.java
+++ src/main/java/com/employeepayrole/EmployeePayroleApplication.java
@@ -1,13 +1,16 @@
 package com.employeepayrole;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayroleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayroleApplication.class, args);
+		log.info("Employee Pay Role app is started");
 	}
 
 }

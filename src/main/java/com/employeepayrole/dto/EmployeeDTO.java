ac8efe4af51f942959327a071a9fbc44e5685648
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-10 16:56:42 +0530

    Able Ensure validations is done on the Payroll DTO

--- src/main/java/com/employeepayrole/dto/EmployeeDTO.java
+++ src/main/java/com/employeepayrole/dto/EmployeeDTO.java
@@ -1,7 +1,12 @@
 package com.employeepayrole.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -9,9 +14,27 @@ import lombok.extern.slf4j.Slf4j;
 @Builder
 @Slf4j
 public class EmployeeDTO {
-
-
+    @Pattern(regexp = "^[A-Z]{1}[a-z\\s]{2,}$",
+            message = "Employee name Invalid")
     private String name;
+    @Min(value = 500,message = "Min vage should 500")
     private long sallary;
 
+    @Pattern(regexp = "male|female",
+            message = "gender need to be male or female")
+    public String gender;
+
+    @JsonFormat(pattern = "dd mm yyyy")
+    @NotNull(message = "start date should not empty")
+    @PastOrPresent(message = "startdate should be past or today")
+    public LocalDate startdate;
+
+    @NotBlank(message = "note can note blank")
+    public String note;
+
+    @NotBlank(message = "profile can note empty")
+    public String profilpic;
+
+    @NotNull(message = "department should not empty")
+    public List<String> department;
 }

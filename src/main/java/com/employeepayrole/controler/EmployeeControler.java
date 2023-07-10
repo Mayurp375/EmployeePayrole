860f582134e274d9ce30e70e82df2f550b8536e4
Mayurp375 <mayurpolojwar1234@gmail.com>
2023-07-10 16:42:33 +0530

    Able to Add remaining properties to         the Payroll DTO and Model

--- src/main/java/com/employeepayrole/controler/EmployeeControler.java
+++ src/main/java/com/employeepayrole/controler/EmployeeControler.java
@@ -3,6 +3,8 @@ package com.employeepayrole.controler;
 import com.employeepayrole.entity.Employee;
 import com.employeepayrole.service.IEmpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,30 +17,35 @@ public class EmployeeControler {
     IEmpService empService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/{name}")
-    public String helow(@RequestBody String name) {
-        return "Hiii" + name + " !";
+    public ResponseEntity<String> helow1(@RequestBody String name) {
+        return new ResponseEntity<>("Hiii" + name + " !", HttpStatus.CREATED);
     }
 
+//    @RequestMapping(method = RequestMethod.GET, path = "/{name}")
+//    public String helow(@RequestBody String name) {
+//        return "Hiii" + name + " !";
+//    }
+
     @GetMapping("/getall")
-    public List<Employee> getall(){
-        return empService.getAll();
+    public ResponseEntity<List<Employee>> getall() {
+        return ResponseEntity.ok(empService.getAll());
     }
 
     @PostMapping("/save")
-    public Employee employee(@RequestBody Employee employee){//jeson
+    public ResponseEntity<Employee> employee(@RequestBody Employee employee) {//jeson
         System.out.println("added successfully");
-        return empService.addData(employee);
+        return ResponseEntity.ok( empService.addData(employee));
     }
 
     @GetMapping("/id/{id}")
-    public Optional<Employee> getEmpById(@PathVariable ("id")int id){
+    public ResponseEntity< Optional<Employee>> getEmpById(@PathVariable("id") int id) {
         System.out.println("find successfully");
-        return empService.getById(id);
+        return ResponseEntity.ok( empService.getById(id));
     }
 
     @GetMapping("/id")
-    public Optional<Employee> getEmpById2(@RequestParam ("id")int id){
+    public ResponseEntity< Optional<Employee>> getEmpById2(@RequestParam("id") int id) {
         System.out.println("find successfully");
-        return empService.getById(id);
+        return ResponseEntity.ok( empService.getById(id));
     }
 }
--- src/main/java/com/employeepayrole/entity/Employee.java
+++ src/main/java/com/employeepayrole/entity/Employee.java
@@ -5,30 +5,45 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
 @Builder
-@AllArgsConstructor
+@AllArgsConstructor(staticName = "build")
 @NoArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public int id;
-    @NotNull
+
+   @NotNull(message = "pass valid name")
     public String name;
-    @NotNull
-    public String contact;
-    @NotNull
+
+    @NotNull(message = "pass valid Contact")
+    public long contact;
+
+    @Min(18)
+    @Max(60)
     public float age;
-    @NotNull
+
+    @Pattern(regexp = "0-9")
     public long sallary;
 
-
+    public String gender;
+    public LocalDate startDate;
+    public String note;
+    public String profile;
+    private List<String> department;
 }
